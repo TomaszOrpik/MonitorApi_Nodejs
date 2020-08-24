fed4023428bd6887fa72d48b12d19400bd99b27f
TomaszOrpik <orpik.tomasz@gmail.com>
2020-08-24 22:36:00 +0200

    New cors...

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', 'https://vegeshop-714fb.web.app/');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
