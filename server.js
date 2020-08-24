f3e879274a4ab02034b7153d5a1de0a1cdf3fdd4
TomaszOrpik <orpik.tomasz@gmail.com>
2020-08-24 23:01:39 +0200

    Another cors...

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const bodyParser = require('body-parser');
 const app = express();
 //app.use(cors());
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://vegeshop-714fb.web.app");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-Width, Content-Type, Accept");
     // Website you wish to allow to connect
     // res.setHeader('Access-Control-Allow-Origin', 'https://vegeshop-714fb.web.app/');
