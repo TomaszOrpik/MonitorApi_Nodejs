e10eead1e52779c26c6bb4232c03e44a7de61329
TomaszOrpik <orpik.tomasz@gmail.com>
2020-08-27 14:58:40 +0200

    Remove cors

--- server.js
+++ server.js
@@ -10,25 +10,25 @@ const bodyParser = require('body-parser');
 
 const app = express();
 //app.use(cors());
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-Width, Content-Type, Accept");
-    // Website you wish to allow to connect
-    // res.setHeader('Access-Control-Allow-Origin', 'https://vegeshop-714fb.web.app/');
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-Width, Content-Type, Accept");
+//     // Website you wish to allow to connect
+//     // res.setHeader('Access-Control-Allow-Origin', 'https://vegeshop-714fb.web.app/');
 
-    // // Request methods you wish to allow
-    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     // // Request methods you wish to allow
+//     // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // // Request headers you wish to allow
-    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//     // // Request headers you wish to allow
+//     // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // // Set to true if you need the website to include cookies in the requests sent
-    // // to the API (e.g. in case you use sessions)
-    // res.setHeader('Access-Control-Allow-Credentials', true);
+//     // // Set to true if you need the website to include cookies in the requests sent
+//     // // to the API (e.g. in case you use sessions)
+//     // res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Pass to next layer of middleware
-    next();
-});
+//     // Pass to next layer of middleware
+//     next();
+// });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( { extended: true }));
 
@@ -47,7 +47,6 @@ db.mongoose
     console.log("Cannot connect to database", err);
     process.exit();
 })
-const Session = db.sessions;
 
 const sessionController = require('./controllers/session.controller');
 app.post("/sessions", sessionController.insertSession);
